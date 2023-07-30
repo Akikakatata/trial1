eb42ea3e486bbb5e7ed0190814a6e1adf1271aae
Akikakatata <Akikakatata0207@gmail.com>
2023-07-30 17:48:20 +0900

    A

--- players/strategy_player.py
+++ players/strategy_player.py
@@ -101,27 +101,6 @@ class StrategicPlayer(Player):
                         return json.dumps(self.move(ship.type, to))
             # If no valid move is found after the maximum attempts, return None
             return None
-        if act == "move":
-            ship = random.choice(list(self.ships.values()))
-            while True:
-                to = random.choice(self.field)
-                while not ship.can_reach(to) or not self.overlap(to) is None:
-                    to = random.choice(self.field)
-                validation = "fit"
-                for i in range(len(self.positions)):
-                    for j in range(i + 1, len(self.positions)):
-                        pos1 = list(to)
-                        pos2 = self.positions[list(self.positions.keys())[j]]
-                        x1, y1 = pos1
-                        x2, y2 = pos2
-                        if ((x1 == x2) or (y1 == y2)) or (abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1):
-                            validation = "unfit"
-                            break
-                    if validation == "unfit":
-                        break
-                if validation == "fit":
-                    return json.dumps(self.move(ship.type, to))
-
         elif act == "attack":
             ship_type = random.choice(list(self.opppnent_possible_positions.keys()))
             to = random.choice(self.opppnent_possible_positions[ship_type])
