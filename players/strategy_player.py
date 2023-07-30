f1a14cd25035877d2cd04662d7e0acd9a49f3db3
Akikakatata <Akikakatata0207@gmail.com>
2023-07-31 06:26:49 +0900

    spell

--- players/strategy_player.py
+++ players/strategy_player.py
@@ -45,7 +45,7 @@ class StrategicPlayer(Player):
                 super().__init__(self.positions)
                 break
 
-        self.opppnent_possible_positions =  []
+        self.opponent_possible_positions =  []
         self.opnnent_certain_positions = []
         self.opponent_HP = 6
         self.player_HP = 6
